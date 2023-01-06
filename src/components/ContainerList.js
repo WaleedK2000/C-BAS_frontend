2aa0f8b5bba6fcc0b4cb4d7d9db795326f7a0ec6
waleedkayani <waleedkayani14@yahoo.com>
2023-01-07 01:19:33 +0500

    Modified code to work with new Django Backend

--- src/components/ContainerList.js
+++ src/components/ContainerList.js
@@ -1,76 +1,76 @@
 // Returns list of containers running in a table, by sending post request to API Server
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const axios = require('axios');
-
-
+const axios = require("axios");
 
 export default function ContainerList() {
+  const [dockerList, setdockerList] = useState(0);
 
-    const [dockerList, setdockerList] = useState(0);
+  useEffect(() => {
+    console.log("sup");
+    axios
+      .get("http://127.0.0.1:8000/api/docker/running_containers", {
+        // params: {
+        //     facultyId: `'AV189'`
+        // },
+      })
+      .then(function (response) {
+        return response.data;
+      })
+      .then((res) => {
+        setdockerList(res);
+        console.log("------------------------------------");
+        console.log(res.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    useEffect(() => {
-        console.log('sup')
-        axios.post('http://localhost:4000/api/fetch/docker/running_containers/', {
-            // params: {
-            //     facultyId: `'AV189'`
-            // },
-        })
-            .then(function (response) {
+  // let table;
 
-                return response.data;
-            })
-            .then(res => {
-                setdockerList(res);
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  // Condition To check if data is loaded
+  if (dockerList === 0) {
+    return <div className="load">Loading</div>;
+  } else if (dockerList.running === 0) {
+    return <div className="load">No Running</div>;
+  } else {
+    const containerNames = Object.keys(dockerList.data);
 
-    // let table;
+    var i = 0;
+    const list = containerNames.map((key) => (
+      <tr key={dockerList.data[key].Name}>
+        <th scope="row">{(i += 1)}</th>
+        <td>{dockerList.data[key].Name}</td>
+        <td>{dockerList.data[key].Config.Image}</td>
+        <td>{dockerList.data[key].State.Status}</td>
+        <td>
+          <button type="button" className="btn btn-link btn-sm" to="/">
+            View
+          </button>
+        </td>
+      </tr>
+    ));
 
+    console.log("heifhrfhrffrfrfrff");
+    console.log(list);
 
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Container Name</th>
+            <th scope="col">Type</th>
+            <th scope="col">Status</th>
 
-    // Condition To check if data is loaded
-    if (dockerList === 0) {
-        return (<div className="load">
-            Loading
-        </div>);
-    } else if (dockerList.running === 0) {
-
-        return (<div className="load">
-            No Running
-        </div>);
-
-    } else {
-        return (
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Container Name</th>
-                        <th scope="col">Type</th>
-                        <th scope="col">Status</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        dockerList.data.map((element) => {
-                            return <tr key={element.Names}>
-                                <th scope="row"></th>
-                                <td>{element.Names}</td>
-                                <td>{element.Image}</td>
-                                <td>{element.State}</td>
-                                <td><button type="button" className="btn btn-link" to>View</button></td>
-                            </tr>
-                        })}
-                </tbody>
-            </table>
-        );
-    }
-
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>{list}</tbody>
+      </table>
+    );
+  }
 }
