ec507c1960fc5a22fcbd9b9d6e962f5cab2e8750
waleedkayani <waleedkayani14@yahoo.com>
2023-04-14 10:02:18 +0500

    added chart on blue docker scan

--- src/components/interface_blue/BlueTeamInterface.jsx
+++ src/components/interface_blue/BlueTeamInterface.jsx
@@ -5,9 +5,13 @@ import BlueTeamConsole from "../blueteam_dashboard/BlueTeamConsole";
 import BlueSidebarComponent from "../global/blueTeam/BlueSidebarComponent";
 import DefenseMatrix from "../blue_scripts/DefenseMatrix";
 import DockerLog from "../blue_scripts/docker_log/DockerLog";
+import BlueVulnerability from "../blue_vulnerability/BlueVulnerability";
 import SelectionContainer from "../selection_container/SelectionContainer";
 import SelectionNode from "../selection_node/SelectionNode";
 import SelectionNodeContainer from "../selection_node_container/SelectionNodeContainer";
+import GeneralBlue from "../general_blue/GeneralBlue";
+
+// Blue Team Interface Routes are stored here
 
 export default function BlueTeamInterface() {
   return (
@@ -16,17 +20,30 @@ export default function BlueTeamInterface() {
         <BlueSidebarComponent />
         <div className="dashboard-main-content">
           <Routes>
+            {/* // Main Console  */}
             <Route path="/blue" element={<BlueTeamConsole />} />
+
+            {/* Defense Matrix Routes */}
             <Route path="/blue/Defense_Matrix" element={<DefenseMatrix />} />
             <Route
               path="/blue/Defense_Matrix/DOCKER_LOGS"
               element={<DockerLog />}
             />
             <Route
+              path="/blue/Defense_Matrix/GENERAL"
+              element={<GeneralBlue />}
+            />
+            <Route
               path="/blue/Defense_Matrix/:attack"
               element={<DefenseMatrix />}
             />
-            <Route path="/blue/test" element={<SelectionNodeContainer />} />
+
+            <Route
+              path="/blue/Defense_Matrix/GENERAL/CONTAINER_SCAN"
+              element={<BlueVulnerability />}
+            />
+
+            <Route path="/blue/test" element={<BlueVulnerability />} />
           </Routes>
         </div>
       </div>
